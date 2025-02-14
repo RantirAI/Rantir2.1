5a6c5007a2e028f0bee4bb7de06117758843b847
wahome mutahi <wahome100@localhost.localdomain>
2025-02-14 19:23:58 +0300

    Updated PagePreview and AddElementCard components

--- web-frontend/modules/builder/components/elements/AddElementCard.vue
+++ web-frontend/modules/builder/components/elements/AddElementCard.vue
@@ -8,43 +8,80 @@
   >
     <div v-if="loading" class="loading"></div>
     <template v-else>
-      <div class="add-element-card__element-type">
-        <i
-          class="add-element-card__element-type-icon"
-          :class="elementType.iconClass"
-        ></i>
-        <span>{{ elementType.name }}</span>
-      </div>
-      <div class="margin-top-1 add-element-card__description">
-        {{ elementType.description }}
+      <div class="add-element-card__content">
+        <div class="add-element-card__element-type">
+          <i
+            class="add-element-card__element-type-icon"
+            :class="elementType.iconClass"
+          ></i>
+        </div>
+        <div class="add-element-card__name">
+          <span>{{ elementType.name }}</span>
+        </div>
       </div>
     </template>
   </div>
-</template>
+ </template>
 
-<script>
-export default {
-  name: 'AddElementCard',
-  props: {
-    elementType: {
-      type: Object,
-      required: true,
-    },
-    loading: {
-      type: Boolean,
-      required: false,
-      default: false,
-    },
-    disabledMessage: {
-      type: String,
-      required: false,
-      default: '',
-    },
-    disabled: {
-      type: Boolean,
-      required: false,
-      default: false,
-    },
-  },
-}
-</script>
+ <script>
+ export default {
+   name: 'AddElementCard',
+   props: {
+     elementType: {
+       type: Object,
+       required: true,
+     },
+     loading: {
+       type: Boolean,
+       required: false,
+       default: false,
+     },
+     disabledMessage: {
+       type: String,
+       required: false,
+       default: '',
+     },
+     disabled: {
+       type: Boolean,
+       required: false,
+       default: false,
+     },
+   },
+ }
+ </script>
+
+ <style scoped>
+ .add-element-card {
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   text-align: center;
+ }
+
+ .add-element-card__content {
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   justify-content: center;
+ }
+
+ .add-element-card__element-type {
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   margin-bottom: 0.5rem;
+ }
+
+ .add-element-card__element-type-icon {
+   font-size: 2rem; /* Adjust size as needed */
+ }
+
+ .add-element-card__name {
+   text-align: center;
+ }
+
+ .add-element-card--disabled {
+   opacity: 0.5;
+   cursor: not-allowed;
+ }
+ </style>
